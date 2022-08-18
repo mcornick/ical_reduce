77610ec0aa7d2e46c1d6f4cf6736fbb0bb7503d6
Mark Cornick <mcornick@mcornick.com>
2022-08-18 00:36:07 -0400

    Use __name__ == "__main__" construct

--- ical_reduce.py
+++ ical_reduce.py
@@ -8,30 +8,35 @@ from sys import argv
 from icalendar import Calendar
 from pytz import utc
 
-try:
-    original_name = argv[1]
-except IndexError:
-    original_name = "original.ics"
-try:
-    reduced_name = argv[2]
-except IndexError:
-    reduced_name = "reduced.ics"
 
-now = datetime.now(utc)
-reduced_cal = Calendar()
+def ical_reduce(original_name, reduced_name):
+    now = datetime.now(utc)
+    reduced_cal = Calendar()
 
-with open(original_name, "rb") as original:
-    original_cal = Calendar.from_ical(original.read())
+    with open(original_name, "rb") as original:
+        original_cal = Calendar.from_ical(original.read())
 
-for event in original_cal.walk():
-    if event.name == "VEVENT":
-        start = event.decoded("dtstart")
-        compare = deepcopy(start)
-        if type(compare) is date:
-            compare = datetime.combine(compare, datetime.min.time())
-        compare = compare.replace(tzinfo=utc)
-        if compare > now:
-            reduced_cal.add_component(event)
+    for event in original_cal.walk():
+        if event.name == "VEVENT":
+            start = event.decoded("dtstart")
+            compare = deepcopy(start)
+            if type(compare) is date:
+                compare = datetime.combine(compare, datetime.min.time())
+            compare = compare.replace(tzinfo=utc)
+            if compare > now:
+                reduced_cal.add_component(event)
 
-with open(reduced_name, "wb") as reduced:
-    reduced.write(reduced_cal.to_ical())
+    with open(reduced_name, "wb") as reduced:
+        reduced.write(reduced_cal.to_ical())
+
+
+if __name__ == "__main__":
+    try:
+        original = argv[1]
+    except IndexError:
+        original = "original.ics"
+    try:
+        reduced = argv[2]
+    except IndexError:
+        reduced = "reduced.ics"
+    ical_reduce(original, reduced)
