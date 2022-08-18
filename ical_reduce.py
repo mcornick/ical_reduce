d4e87fc4bc8a5ca1c2068a9eef947ed7bb576f3c
Mark Cornick <mcornick@mcornick.com>
2022-08-18 00:44:52 -0400

    Handle file exceptions

--- ical_reduce.py
+++ ical_reduce.py
@@ -13,8 +13,21 @@ def ical_reduce(original_name, reduced_name):
     now = datetime.datetime.now(pytz.utc)
     reduced_cal = icalendar.Calendar()
 
-    with open(original_name, "rb") as original:
-        original_cal = icalendar.Calendar.from_ical(original.read())
+    try:
+        with open(original_name, "rb") as original:
+            original_cal = icalendar.Calendar.from_ical(original.read())
+    except FileNotFoundError:
+        sys.stderr.write("{} not found\n".format(original_name))
+        sys.exit(1)
+    except IsADirectoryError:
+        sys.stderr.write("{} is a directory\n".format(original_name))
+        sys.exit(1)
+    except PermissionError:
+        sys.stderr.write("No permission to read {}\n".format(original_name))
+        sys.exit(1)
+    except OSError:
+        sys.stderr.write("OS error trying to read {}\n".format(original_name))
+        sys.exit(1)
 
     for event in original_cal.walk():
         if event.name == "VEVENT":
@@ -28,8 +41,18 @@ def ical_reduce(original_name, reduced_name):
             if compare > now:
                 reduced_cal.add_component(event)
 
-    with open(reduced_name, "wb") as reduced:
-        reduced.write(reduced_cal.to_ical())
+    try:
+        with open(reduced_name, "wb") as reduced:
+            reduced.write(reduced_cal.to_ical())
+    except IsADirectoryError:
+        sys.stderr.write("{} is a directory\n".format(reduced_name))
+        sys.exit(1)
+    except PermissionError:
+        sys.stderr.write("No permission to write {}\n".format(reduced_name))
+        sys.exit(1)
+    except OSError:
+        sys.stderr.write("OS error trying to write {}\n".format(reduced_name))
+        sys.exit(1)
 
 
 if __name__ == "__main__":
